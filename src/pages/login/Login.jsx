43c5b37367838b3105f05227357aa2303ee8dcac
jrnohm <rtchphm@gmail.com>
2025-02-15 20:58:14 +0700

    collision push

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -20,6 +20,7 @@ function Login() {
         </div>
       </div> */}
       <div>ทดสอบ</div>
+      <div>ทดสอบชนกัน</div>
       <div className="place-content-center bg-cover  w-full  min-h-[100vh] bg-[#93cbee39]">
         <div className="mx-[20px] flex justify-center">
           <img
